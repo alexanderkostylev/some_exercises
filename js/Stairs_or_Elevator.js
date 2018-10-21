6379a303240015f82b4dc01eed7722217558e994
alexanderkostylev <alexander.rels01@yandex.com>
2018-10-22 03:09:03 +0500

    add new codeforces challenge

---
+++ js/Stairs_or_Elevator.js
@@ -0,0 +1,33 @@
+
+http://codeforces.com/contest/1054/problem/A
+
+function check(arr) {
+  arr.forEach(element => {
+    Math.abs(element); 
+  });
+  const [inFloor, outFloor, elevatorFloor, timeStairs, timeElevator, timeDoors] = arr;
+  let floorCount = outFloor - inFloor;
+  floorCount = Math.abs(floorCount);
+
+  function calcElevator(){
+    let targetFloor = inFloor > elevatorFloor ? inFloor - elevatorFloor : elevatorFloor - inFloor;
+    let targetFloorTime = targetFloor * timeElevator;
+
+    return targetFloorTime + timeDoors*2 + timeElevator*floorCount ;
+  }
+
+  function calcStairs(){
+    return timeStairs * floorCount;
+  }
+
+  // True если на лифте быстрее, иначе false 
+  return calcElevator() < calcStairs();
+}
+
+function main(str) {
+  str =  str || prompt();
+  const arr = str.split(' ');
+  const output = check(arr) ? 'YES' : 'NO';
+  console.log(output);
+}
+main();
